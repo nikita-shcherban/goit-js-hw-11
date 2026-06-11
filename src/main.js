daedab3dc26974348c65a68c1063e93c8a26c8b0
NikitaShcherban <cruizsports@gmail.com>
2026-06-11 18:46:03 +0300

    complete

--- src/main.js
+++ src/main.js
@@ -1,1 +1,46 @@
-import axios from 'axios';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import {
+  clearGallery,
+  createGallery,
+  hideLoader,
+  showLoader,
+} from './js/render-functions';
+import { getImagesByQuery } from './js/pixabay-api';
+
+const formSearch = document.querySelector('.form');
+
+formSearch.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const searchQuery = event.currentTarget.elements['search-text'].value.trim();
+
+  if (!searchQuery) {
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  getImagesByQuery(searchQuery)
+    .then(response => {
+      if (response.hits.length === 0) {
+        iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+        return;
+      }
+      createGallery(response.hits);
+    })
+    .catch(error => {
+      console.error(error);
+      hideLoader();
+    })
+    .finally(() => {
+      hideLoader();
+      formSearch.reset();
+    });
+}
